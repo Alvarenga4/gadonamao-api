915c934dba59aaa1b9b087645371a04eb3653e72
Alex Sandro <alexss050786@gmail.com>
2021-06-02 14:00:08 -0300

    add routes

--- router/index.routes.js
+++ router/index.routes.js
@@ -7,6 +7,46 @@ router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'site', 'index.html'));
 });
 
+router.get('/:categoria', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'site', 'index.html'));
+});
+
+router.get('/anuncio/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'site', 'index.html'));
+});
+
+router.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'site', 'index.html'));
+});
+
+router.get('/login-admin', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'site', 'index.html'));
+});
+
+router.get('/cadastro', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'site', 'index.html'));
+});
+
+router.get('/esquecisenha', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'site', 'index.html'));
+});
+
+router.get('/depoimentos', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'site', 'index.html'));
+});
+
+router.get('/cotacao-novilha', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'site', 'index.html'));
+});
+
+router.get('/cotacao-boigordo', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'site', 'index.html'));
+});
+
+router.get('/alterarsenha/:token', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'site', 'index.html'));
+});
+
 router.get('/admin', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'admin', 'index.html'));
 });
@@ -15,11 +55,87 @@ router.get('/admin/:token', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'admin', 'index.html'));
 });
 
+router.get('admin/usuarios', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'admin', 'index.html'));
+});
+
+router.get('admin/sliders', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'admin', 'index.html'));
+});
+
+router.get('admin/categorias', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'admin', 'index.html'));
+});
+
+router.get('admin/categorias/categoria', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'admin', 'index.html'));
+});
+
+router.get('admin/categorias/categoria/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'admin', 'index.html'));
+});
+
+router.get('admin/idades', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'admin', 'index.html'));
+});
+
+router.get('admin/unidades', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'admin', 'index.html'));
+});
+
+router.get('admin/pelagems', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'admin', 'index.html'));
+});
+
+router.get('admin/andrologias', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'admin', 'index.html'));
+});
+
+router.get('admin/pesos', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'admin', 'index.html'));
+});
+
+router.get('admin/litros', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'admin', 'index.html'));
+});
+
+router.get('admin/registros', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'admin', 'index.html'));
+});
+
+router.get('admin/depoimentos', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'admin', 'index.html'));
+});
+
 router.get('/admin-usuario', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'adminuser', 'index.html'));
 });
 
 router.get('/admin-usuario/:token', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'adminuser', 'index.html'));
+});
+
+router.get('admin-usuario/perfil', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'adminuser', 'index.html'));
+});
+
+router.get('admin-usuario/anuncios', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'adminuser', 'index.html'));
+});
+
+router.get('admin-usuario/anuncio', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'adminuser', 'index.html'));
+});
+
+router.get('admin-usuario/anuncio-detalhes', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'adminuser', 'index.html'));
+});
+
+router.get('admin-usuario/anuncio-video-fotos/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'adminuser', 'index.html'));
+});
+
+router.get('admin-usuario/animais', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'adminuser', 'index.html'));
 });
 
