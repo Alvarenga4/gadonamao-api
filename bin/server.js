b48824bfff0ac23256d148ec380a025e11d1ab23
Francisco Alvarenga <francisco.alvarenga@vitta.me>
2021-06-14 15:34:13 -0300

    chore: chage https to http

--- bin/server.js
+++ bin/server.js
@@ -4,10 +4,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
-const server = require('https').createServer({
-  key: fs.readFileSync(path.join(__dirname, '..', 'cert', 'key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, '..', 'cert', 'cert.pem'))
-}, app);
+
+const server = require('http').Server(app);
+
+// const server = require('https').createServer({
+//   key: fs.readFileSync(path.join(__dirname, '..', 'cert', 'key.pem')),
+//   cert: fs.readFileSync(path.join(__dirname, '..', 'cert', 'cert.pem'))
+// }, app);
 const debug = require('debug')('integracao:server');
 const loaders = require('../src/loaders');
 
